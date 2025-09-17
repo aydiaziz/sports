915a5286d99f7c71da0cacee7d208bfdc689199a
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-09-17 13:32:42 +0100

    fixation de la bd et settings

--- backend/sports/settings.py
+++ backend/sports/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = 'django-insecure-i)raxg*(azsovf^b4!ixjd%88#j6_s&e#7yxhc4#%src@u77b(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [ 'localhost']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:4200"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt.token_blacklist', 
+    'corsheaders',
     'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,8 +82,12 @@ WSGI_APPLICATION = 'sports.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sports',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': '127.0.0.1',  
+        'PORT': '5432',
     }
 }
 
