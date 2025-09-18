912804aa630beaf1aa18dc430f92acf316e760b7
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-09-18 14:05:47 +0100

    Fix AuthGuard canActivateChild implementation

--- frontend/sports/src/app/auth/guards/auth.guard.ts
+++ frontend/sports/src/app/auth/guards/auth.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable, map } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): Observable<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | boolean | UrlTree {
     if (!this.authService.isAuthenticated()) {
       return this.router.parseUrl('/auth/login');
     }
@@ -16,5 +26,12 @@ export class AuthGuard implements CanActivate {
     return this.authService.ensureProfileLoaded().pipe(
       map((isLoaded) => (isLoaded ? true : this.router.parseUrl('/auth/login')))
     );
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivate(route, state);
   }
 }
